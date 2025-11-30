1f3495c96821d57a726c3502d0750d1cff0bbdbe
rizkurniawan <rizkykurniawan2023@gmail.com>
2025-11-30 10:10:51 +0700

    learn how to create a field in class and use in the object (assign value) and get value

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -2,6 +2,14 @@ public class PersonApp {
     public static void main(String[] args) {
         // create object
         var person1 = new Person();
+        person1.name = "Rizky";
+        person1.address = "Jakarta";
+        // person1.country = "Amerika"; // ERROR
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
         Person person2 = new Person();
 
     }
