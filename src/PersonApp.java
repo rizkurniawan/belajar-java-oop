3b357894e524189aa2b567e0e4583c4bf6b43dfd
rizkurniawan <rizkykurniawan2023@gmail.com>
2025-11-30 09:54:41 +0700

    learn how to create object

---
+++ src/PersonApp.java
@@ -0,0 +1,8 @@
+public class PersonApp {
+    public static void main(String[] args) {
+        // create object
+        var person1 = new Person();
+        Person person2 = new Person();
+
+    }
+}
