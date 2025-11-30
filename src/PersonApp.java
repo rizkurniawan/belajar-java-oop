f5cef55cd0abb5977cd48ecadd41079dbb5deabf
rizkurniawan <rizkykurniawan2023@gmail.com>
2025-11-30 10:44:08 +0700

    learn constructor overloading

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -8,5 +8,11 @@ public class PersonApp {
         System.out.println(person1.country);
 
         person1.sayHello("Eko");
+
+        Person person2 = new Person("Khannedy");
+
+        System.out.println(person2.name);
+        System.out.println(person2.address);
+        System.out.println(person2.country);
     }
 }
