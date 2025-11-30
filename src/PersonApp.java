6cd47983cbdb8411e4e70f65b6ad9b1b103df7b2
rizkurniawan <rizkykurniawan2023@gmail.com>
2025-11-30 10:30:07 +0700

    learn how to make a constructor

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,18 +1,12 @@
 public class PersonApp {
     public static void main(String[] args) {
         // create object
-        var person1 = new Person();
-        person1.name = "Rizky";
-        person1.address = "Jakarta";
-        // person1.country = "Amerika"; // ERROR
+        Person person1 = new Person("Rizky", "Jakarta");
 
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
 
         person1.sayHello("Eko");
-
-        Person person2 = new Person();
-
     }
 }
