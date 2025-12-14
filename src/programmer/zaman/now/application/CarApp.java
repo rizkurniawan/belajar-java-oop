44436146e91f1b8598f26f668880ba77b82d911b
rizkurniawan <rizkykurniawan2023@gmail.com>
2025-12-14 14:50:15 +0700

    belajar interface

---
+++ src/programmer/zaman/now/application/CarApp.java
@@ -0,0 +1,12 @@
+package programmer.zaman.now.application;
+
+import programmer.zaman.now.data.Avanza;
+import programmer.zaman.now.data.Car;
+
+public class CarApp {
+    public static void main(String[] args) {
+        Car car = new Avanza();
+        car.drive();
+        System.out.println(car.getTire());
+    }
+}
---
+++ src/programmer/zaman/now/data/Car.java
@@ -0,0 +1,6 @@
+package programmer.zaman.now.data;
+
+public interface Car {
+    void drive();
+    int getTire();
+}
